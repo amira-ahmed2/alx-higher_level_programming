1dcaf5e70a56a6cc8e2ddffa3886b42a57c975d8
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-10-31 20:40:33 +0200

    Create 7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def islower(c):
+    for i in range(97, 123):
+        if ord(c) == i:
+            return True
+        else:
+            continue
+        
+    return False
