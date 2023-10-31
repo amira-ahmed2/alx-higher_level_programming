8520f55b852c39004e1b3180a1852c035f228987
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-10-31 11:22:27 -0700

    add

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-for x in range(9):
-    for y in range(1, 10):
-        if x == y or x > y:
-            continue
-        if x == 8:
-            print("{:d}{:d}".format(x, y))
-        else:
-            print("{:d}{:d}".format(x, y), end=', ')
