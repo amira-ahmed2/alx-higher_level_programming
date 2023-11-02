a5e03ea4e2baa3982572b87146ed9839b5cb2a6f
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-11-02 21:53:57 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num_avg = len(sys.argv) - 1
+    number = len(sys.argv) - 1
 
-    if num_avg == 0:
-        print("{} arguments:".format(num_avg))
-    elif num_avg == 1:
-        print("{} arguments:".format(num_avg))
+    if number == 1:
+        print("{} arguments.".format(number))
+    elif number == 0:
+        print("{} argument:".format(number))
     else:
-        print("{} arguments:".format(num_avg))
+        print("{} argument:".format(number))
 
-    if num_avg >= 1:
-        num_avg = 0
+    if number >= 1:
+        number = 0
         for i in sys.argv:
-            if num_avg != 0:
-                print("{}: {}".format(num_avg, i))
-            num_avg += 1
+            if number != 0:
+                print("{}: {}".format(number, i))
+            number += 1
