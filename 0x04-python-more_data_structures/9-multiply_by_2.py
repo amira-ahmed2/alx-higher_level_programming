82c4c2e7975e5182c16a5e804358194944ef9376
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-11-09 00:02:45 +0200

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dirc = {}
-    for k,x in a_dictionary.items():
+    for k, x in a_dictionary.items():
         new_dirc[k] = x * 2
     return new_dirc
