c102d32bdb3629562267a3f9877c691b58131c8b
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2023-11-09 02:31:34 +0200

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    else:
+        result = 0
+        value_befor = 0
+        roman_number = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for char in reversed(roman_string):
+            value_reversed = roman_number.get(char, 0)
+            if value_reversed < value_befor:
+                result -= value_reversed
+            else:
+                result += value_reversed
+            value_befor = value_reversed
+        return result
