b3c1e33be678d952964fb70ed313b66c55404b22
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-20 17:38:46 +0200

    add

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             nb_print += 1
     except IndexError:
-        pass
+        print(end="")
     finally:
         print()
     return nb_print
