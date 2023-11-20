3035e831f59f3eaeb6d7f544bba09f5d17123e6c
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-20 19:19:55 +0200

    add

--- 0x05-python-exceptions/mainTest.py
+++ 0x05-python-exceptions/mainTest.py
@@ -14,7 +14,7 @@ nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
 """
 # task 1
-
+"""
 safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
 value = 89
@@ -31,3 +31,19 @@ value = "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
+"""
+# task2
+safe_print_list_integers = \
+    __import__('2-safe_print_list_integers').safe_print_list_integers
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
