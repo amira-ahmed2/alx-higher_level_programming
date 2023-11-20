24684a8a82608d861633bac422029b6802f48ba0
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-20 20:36:12 +0200

    add

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError):
+        import sys
+        print("Exception: Could not print the value as an integer", file=sys.stderr)
         return False
     else:
         return True
