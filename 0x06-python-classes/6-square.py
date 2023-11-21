9d04cbb611ac6e4a010567c3f556b1343714005e
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-21 18:31:28 +0200

    add

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,8 +13,8 @@ class Square:
         Args:
             size (int): size of the square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """get area Square
