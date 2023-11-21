1c8d1f788f4e3c8421fadde65a48556b9505396a
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-21 16:58:11 +0200

    add

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defines class Square """
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of  the square
+    """
+    def __init__(self, size=0):
+        """init square
+
+        Args:
+            size (int): size of the square
+        """
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
