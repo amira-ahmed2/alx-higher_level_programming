f88ab9053180b08b337d126eb3d314912edcc335
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-22 00:32:28 +0200

    add

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -70,11 +70,8 @@ class Square:
         if self.__size == 0:
             print()
 
-        for ii in range(0, self.__position[1]):
-            print("")
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            for j in range(0, self.__position[0]):
-                print("_", end="")
-            for a in range(0, self.__size):
-                print("#", end="")
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
