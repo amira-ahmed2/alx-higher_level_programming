e692008465d43c22eec91cd129a7ef1835509003
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-24 15:13:30 +0200

    add

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines an integer addition function"""
+
+
+def add_integer(a, b=98):
+    """that fun is sum two integer
+
+    Args:
+        a (int or float): one number
+        b (int, float): two number. Defaults to 98.
+
+    Raises:
+        TypeError: a must be an integer
+        TypeError: b must be an integer
+
+    Returns:
+        int: sun two numbers
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
