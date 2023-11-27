1330029b5d585a7f2be9f6993d9c56462ae2557b
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-27 17:05:58 +0200

    add

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-Rectangle = __import__('0-rectangle').Rectangle
+# Rectangle = __import__('0-rectangle').Rectangle
 
 # my_rectangle = Rectangle()
 # print(type(my_rectangle))
 # print(my_rectangle.__dict__)
 
+# my_rectangle = Rectangle(2, 4)
+# print(my_rectangle.__dict__)
+
+# my_rectangle.width = 10
+# my_rectangle.height = 3
+# print(my_rectangle.__dict__)
+
+Rectangle = __import__('3-rectangle').Rectangle
+
 my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
 
 my_rectangle.width = 10
 my_rectangle.height = 3
-print(my_rectangle.__dict__)
+print(my_rectangle)
+print(repr(my_rectangle))
