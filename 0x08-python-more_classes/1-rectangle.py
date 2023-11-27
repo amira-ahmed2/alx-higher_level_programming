1e4f5c58ffc12f72bbd5e5b77ce727827670f465
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-27 16:43:18 +0200

    add

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,8 +33,8 @@ class Rectangle:
             raise TabError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+
+        self.__width = value
 
     @property
     def height(self):
@@ -56,5 +56,4 @@ class Rectangle:
             raise TabError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
