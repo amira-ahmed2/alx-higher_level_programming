278056b798234e10d007e928efaf4eb05b11fd88
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-27 16:38:36 +0200

    add

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" A class empty class Rectangle"""
+
+
+class Rectangle:
+    """ A class empty class Rectangle"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """retrieves width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width
+
+        Args:
+            value (int): property
+        """
+        if value is not int:
+            raise TabError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """retrieves height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height
+
+        Args:
+            value (int): property
+        """
+        if value is not int:
+            raise TabError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
