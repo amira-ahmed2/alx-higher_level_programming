aba6f375133a65f3445cdc5277b5f4bb11e26e4c
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-27 17:44:19 +0200

    add

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -12,17 +12,29 @@
 # my_rectangle.height = 3
 # print(my_rectangle.__dict__)
 
-Rectangle = __import__('3-rectangle').Rectangle
+# Rectangle = __import__('3-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+# my_rectangle = Rectangle(2, 4)
+# print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
-print(str(my_rectangle))
-print(repr(my_rectangle))
+# print(str(my_rectangle))
+# print(repr(my_rectangle))
 
-print("--")
+# print("--")
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
+# my_rectangle.width = 10
+# my_rectangle.height = 3
+# print(my_rectangle)
+# print(repr(my_rectangle))
+
+#!/usr/bin/python3
+Rectangle = __import__('6-rectangle').Rectangle
+
+my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_2 = Rectangle(2, 4)
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_1
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_2
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+
