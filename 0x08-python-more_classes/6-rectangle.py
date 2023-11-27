df0902aaa47d3d1946add448d93ac13e12d2ea93
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-27 17:52:37 +0200

    add

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,8 +4,8 @@
 
 class Rectangle:
     """this represents a rectangle"""
-
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initializing this rectangle class
         Args:
