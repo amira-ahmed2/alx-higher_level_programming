e1f1af6ecb2183c7b8cda528b0cf5d41232ea58d
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-11-27 17:48:14 +0200

    add

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -68,5 +68,6 @@ class Rectangle:
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
+        """prints a message for every object that is deleted"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
