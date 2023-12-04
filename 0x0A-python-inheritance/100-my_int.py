3c32be77eb17b40c797e681cc85058131d66f226
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-12-04 19:07:03 +0200

    add

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,8 +5,10 @@
 class MyInt(int):
     """Write a class MyInt that inherits from int"""
     def __eq__(self, value):
-        return(self.real != value)
+        """Override == opeartor with != behavior"""
+        return super().__ne__(value)
     
     def __ne__(self, value):
+        """Override != opeartor with == behavior"""
         return super().__eq__(value)
         
