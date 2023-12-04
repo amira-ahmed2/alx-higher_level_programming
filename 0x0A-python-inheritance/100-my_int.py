c2c6cc063e77651c022f50cdd47933d532c2b32c
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-12-04 19:04:09 +0200

    add

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,6 +7,6 @@ class MyInt(int):
     def __eq__(self, value):
         return(self.real != value)
     
-    def __ne__(self, __value: object):
-        return super().__eq__(__value)
+    def __ne__(self, value):
+        return super().__eq__(value)
         
