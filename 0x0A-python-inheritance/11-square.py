e2a55046114790e2597a504b6d80e98d4b238562
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-12-04 18:39:55 +0200

    add

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Square"""
+rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(rectangle):
+    """this is an class"""
+    def __init__(self, size, height):
+        """intialize Square
+
+        Args:
+            size (int): _description_
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
