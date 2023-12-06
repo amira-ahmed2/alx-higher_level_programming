fd0f0b6c342e93507aeea1bc92088ca2fa3fce93
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-12-07 01:41:20 +0200

    add

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,6 +9,6 @@ def write_file(filename="", text=""):
         filename (str, optional): _description_. Defaults to "".
         text (str, optional): _description_. Defaults to "".
     """
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         c = f.write(text)
     return c
