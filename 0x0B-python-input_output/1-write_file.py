5470ab8a46c09d7139702f45824eacabbda339d0
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-12-07 01:40:30 +0200

    add

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""write file"""
+
+
+def write_file(filename="", text=""):
+    """fun is write file
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        c = f.write(text)
+    return c
