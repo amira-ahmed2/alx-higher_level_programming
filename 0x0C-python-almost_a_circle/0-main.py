79df926ca37f4a15af15aa4989e0e5a53d8ae350
amira-ahmed2 <amiraa.amin300@yahoo.com>
2023-12-10 20:19:48 +0200

    add

--- 0x0C-python-almost_a_circle/0-main.py
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -1,20 +1,97 @@
+# #!/usr/bin/python3
+# """ 12-main """
+# from models.rectangle import Rectangle
+
+# if __name__ == "__main__":
+
+#     r1 = Rectangle(10, 2, 1, 9)
+#     print(r1)
+#     r1_dictionary = r1.to_dictionary()
+#     print(r1_dictionary)
+#     print(type(r1_dictionary))
+
+#     r2 = Rectangle(1, 1)
+#     print(r2)
+#     r2.update(**r1_dictionary)
+#     print(r2)
+#     print(r1 == r2)
 #!/usr/bin/python3
-""" 0-main """
-from models.base import Base
+""" 13-main 
+from models.square import Square
 
 if __name__ == "__main__":
 
-    b1 = Base()
-    print(b1.id)
+    s1 = Square(10, 2, 1)
+    print(s1)
+    s1_dictionary = s1.to_dictionary()
+    print(s1_dictionary)
+    print(type(s1_dictionary))
 
-    b2 = Base()
-    print(b2.id)
+    s2 = Square(1, 1)
+    print(s2)
+    s2.update(**s1_dictionary)
+    print(s2)
+    print(s1 == s2)
+"""
+#!/usr/bin/python3
+""" 11-main 
+from models.square import Square
 
-    b3 = Base()
-    print(b3.id)
+if __name__ == "__main__":
 
-    b4 = Base(12)
-    print(b4.id)
+    s1 = Square(5)
+    print(s1)
 
-    b5 = Base()
-    print(b5.id)
+    s1.update(10)
+    print(s1)
+
+    s1.update(1, 2)
+    print(s1)
+
+    s1.update(1, 2, 3)
+    print(s1)
+
+    s1.update(1, 2, 3, 4)
+    print(s1)
+
+    s1.update(x=12)
+    print(s1)
+
+    s1.update(size=7, y=1)
+    print(s1)
+
+    s1.update(size=7, id=89, y=1)
+    print(s1)
+    """
+    #!/usr/bin/python3
+""" 10-main 
+from models.square import Square
+
+if __name__ == "__main__":
+
+    s1 = Square(5)
+    print(s1)
+    print(s1.size)
+    s1.size = 10
+    print(s1)
+
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+"""
+
+#!/usr/bin/python3
+""" 14-main """
+from models.base import Base
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 7, 2, 8)
+    dictionary = r1.to_dictionary()
+    json_dictionary = Base.to_json_string([dictionary])
+    print(dictionary)
+    print(type(dictionary))
+    print(json_dictionary)
+    print(type(json_dictionary))
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 """class base"""
+import json
 
 
 class Base:
+    """base"""
     __nb_objects = 0
     def __init__(self, id=None):
+        """base
+
+        Args:
+            id (_type_, optional): _description_. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects 
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
