79f6b0359c0117816adce2e2a3d1b488f89b94c8
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-12 20:50:56 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const x = Number(process.argv[2]);
+  const number = parseInt(process.argv[2]);
   let i = 0;
-  while (i < x) {
+  while (i < number) {
     console.log('C is fun');
     i++;
   }
