7ff7cb3f6302c0002c487a7c5f17b71373946391
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-12 21:25:51 +0200

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 function factorial (num) {
   if (num < 0) {
     return (-1);
@@ -6,7 +7,7 @@ function factorial (num) {
   if (num === 0 || isNaN(num)) {
     return (1);
   }
-  return (n * factorial(num - 1));
+  return (num * factorial(num - 1));
 }
 
 console.log(factorial(Number(process.argv[2])));
