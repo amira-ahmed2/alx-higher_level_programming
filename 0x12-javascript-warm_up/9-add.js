8555ca451ad4d7a5d3a706ef4cd2110b25a4735e
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-12 21:14:40 +0200

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-function add(a, b) {
-    console.log( a + b);
+function add (a, b) {
+  const sum = a + b;
+  console.log(sum);
 }
 add(Number(process.argv[2]), Number(process.argv[3]));
