43e26d4f806246d78ac6f8a59e7f91243d0f9bab
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-02-12 20:46:06 +0200

    update

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let i = number 
+i = 6
+if (!isNaN(i)) {
+	  while (i) {
+		      console.log("C is fun");
+		      i --;
+	  }
+} else {
+	  console.log("Missing number of occurrences");
+}
