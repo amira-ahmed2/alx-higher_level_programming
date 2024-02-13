f680ca3a5f44d63f77f0a5816ecfa42e87a746f7
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-13 18:05:16 +0200

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const Squareone = require('./5-square');
+const SquareOne = require('./5-square');
 
-class Square extends Squareone {
+class Square extends SquareOne {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width))
+      console.log(c.repeat(this.width));
     }
   }
 }
