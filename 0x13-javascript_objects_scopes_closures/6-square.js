a4ffbf2247228fe894913ed5e849df5ed99fbf6a
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-13 18:03:59 +0200

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ class Square extends Squareone {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width))
+      console.log(c.repeat(this.width))
     }
   }
 }
