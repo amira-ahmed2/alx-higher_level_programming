0c0302381e0baee39c033ddb5f7541807e9518f4
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-13 17:28:38 +0200

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-
-    print() {
-        if (this.height && this.width) {
-            for(let i = 0; i < this.height; i++) {
-                console.log('X'.repeat(this.width));
-            }
-        }
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
+  print() {
+      if (this.height && this.width) {
+        for(let i = 0; i < this.height; i++) {
+           console.log('X'.repeat(this.width));
+        }
+      }
+    }
+  }
+module.exports = Rectangle;
