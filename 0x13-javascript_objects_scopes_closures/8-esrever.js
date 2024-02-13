4de53e9743230fd661fe8b1d1b166978782dcf3d
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-13 19:26:11 +0200

    Create 8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  reverse_list = []
+  for (let i = list.length - 1; i >= 0; i--) {
+    reverse_list.push(list[i])
+  }
+  return reverse_list
+};
