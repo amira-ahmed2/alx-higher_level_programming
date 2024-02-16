0be71e1b1573b058f0fce31d856b27718f2bfa82
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-16 20:33:26 +0200

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+This lists all states with a
+name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
