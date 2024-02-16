8bbecf52f3e24acf92eb8317547975628f12d805
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-16 20:39:35 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """
-This lists all states from
-the database hbtn_0e_0_usa.
+This that takes in arguments and
+displays all values in
+the states table of hbtn_0e_0_usa
+where name matches the argument.
+But this time, write one that is safe
+from MySQL injections!
 """
 
 import MySQLdb
