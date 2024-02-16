c2b757ad796f5fd00500d0e46b6ee648f3df690d
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-02-17 01:30:19 +0200

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,7 +12,6 @@ Base = declarative_base()
 
 class State(Base):
     """State class
-
     Attributes:
         __tablename__ (str): The table name of the class
         id (int): The State id of the class
@@ -20,5 +19,6 @@ class State(Base):
 
     """
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
