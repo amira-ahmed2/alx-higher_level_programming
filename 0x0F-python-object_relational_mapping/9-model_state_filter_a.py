0a6dc49cdfa7482c3cf03bba28e326cc5005258b
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-02-17 12:58:47 +0200

    update

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,14 +12,16 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Access to the database.
+    Access to the database and get a state
+    from the database.
     """
 
-    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db)
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    for inst in session.query(State).filter(State.name.contains('a')):
-        print('{0}: {1}'.format(inst.id, inst.name))
+    for instance in session.query(State).filter(State.name.contains('a')):
+        print('{0}: {1}'.format(instance.id, instance.name))
