f3983396f8eb1321f41f221a214c03b4e836531f
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-02-29 22:55:47 +0200

    update

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Finds a peak in a list of unsorted integers
-"""
+""" Test function find_peak """
+find_peak = __import__('6-peak').find_peak
 
-
-def find_peak(list_of_integers):
-    """
-    """
-    if len(list_of_integers) == 0:
-        return None
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
-""" Finds a peak in a list of unsorted integers
+""" finds a peak in a list of unsorted integers.
 """
 
 
 def find_peak(list_of_integers):
+    """that finds a peak in a list of unsorted integers.
     """
-    """
-    
+    length = len(list_of_integers)
+    s = 0
+    e = length - 1
+    if length == 0:
+        return None
+    elif length == 1:
+        return list_of_integers[0]
+
+    while s <= e:
+        mid = int(e + s)
+        last_value = list_of_integers[mid]
+        l_value = list_of_integers[mid - 1]  if mid - 1 >= 0 else float("-inf")
+        r_value = list_of_integers[mid + 1]  if mid + 1 < length else float("-inf")
+        if l_value > last_value:
+            return l_value
+        elif r_value > last_value:
+            return r_value
+        else:
+            return last_value
