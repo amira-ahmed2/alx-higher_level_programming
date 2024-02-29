609880ded9af2b59162bdf974cc1d646f876061a
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-29 21:54:54 +0200

    add

---
+++ 0x10-python-network_0/6-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    """
+    if len(list_of_integers) == 0:
+        return None
