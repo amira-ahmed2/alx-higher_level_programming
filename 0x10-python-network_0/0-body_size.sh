03c971c1bbb4c943f86a4748bb623baae0a7b57e
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-29 18:57:56 +0200

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# That takes in a URL, sends a request to that URL
 curl -s "$1" | wc -c
