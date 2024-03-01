dac038a3dcf55bbc0990a1288ac6658bae9f1e78
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-03-01 18:51:05 +0200

    update

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-import urllib.request
+"""
+that takes in a URL and an email address,
+sends a POST request to the passed
+URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import sys
+import requests
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    url = sys.argv[1]
+    data = {"email": sys.argv[2]}
+    req = requests.post(url, data)
+    print(req.text)
