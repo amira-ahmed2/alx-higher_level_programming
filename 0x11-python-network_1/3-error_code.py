c2858331b885347fe7c021c92a2f5d5fcb8abd43
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-03-01 18:24:25 +0200

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,15 +3,17 @@
 the URL and displays the body of
 the response (decoded in utf-8).
 """
+
+
 import sys
-import urllib.parse
 import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(values).encode('ascii')
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
