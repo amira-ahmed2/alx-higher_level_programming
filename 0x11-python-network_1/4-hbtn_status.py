f60a156b7f11f6ebd62b7339c5a5df27e467026b
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-03-01 18:37:22 +0200

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
+"""
+that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
